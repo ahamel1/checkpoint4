62a2d1dbe550512ec603b825602ac915e6541434
ahamel1 <ahamel.wcs44@gmail.com>
2020-07-23 11:27:35 +0200

    get and post route done

--- back/index.js
+++ back/index.js
@@ -1,10 +1,51 @@
 const express = require("express");
-const { response } = require("express");
+const cors = require("cors");
 const app = express();
 const port = 8000;
 
+const connection = require("./conf");
+
+app.use(cors());
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
 app.get("/", (req, res) => {
-  response.send("Bienvenue sur Express");
+  res.send("Bienvenue sur Express");
+});
+
+//GET ALL PERSON
+app.get("/contact", (req, res) => {
+  connection.query("SELECT * FROM person", (err, results) => {
+    if (err) {
+      res.status(500).json({
+        message: "Erreur lors de l'enregistrement de vos données",
+        error: err,
+      });
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+//POST NEW PERSON
+app.post("/contact", (req, res) => {
+  const formData = req.body;
+  connection.query("INSERT INTO person SET ?", formData, (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({
+        message: "Erreur lors de l'enregistrement de vos données",
+        error: err,
+      });
+    } else {
+      res.sendStatus(201);
+    }
+  });
 });
 
 app.listen(port, (err) => {
