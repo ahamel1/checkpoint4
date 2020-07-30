da046d95547042d72e5d2d1af71411c6c73e3872
ahamel1 <ahamel.wcs44@gmail.com>
2020-07-30 09:45:53 +0200

    component skill add

--- front/src/components/Skills.jsx
+++ front/src/components/Skills.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { TitleH2 } from "../titles";
 
 export default function Skills() {
-  return <></>;
+  return (
+    <div style={{ marginBottom: "9rem" }}>
+      <TitleH2 mobilemt id="mesprojets">
+        Skills
+      </TitleH2>
+    </div>
+  );
 }
